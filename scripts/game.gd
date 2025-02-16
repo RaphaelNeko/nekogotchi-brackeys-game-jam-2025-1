5733495f00d196f7991a88b7ac4f9fe7ef7d334e
Raphael Neko <contact@raphaelneko.com>
2025-02-16 17:42:23 +0100

    New sfx imported + button signals added

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var intro_animation: AnimationPlayer
-@export var nekogotchi_root: Node3D
+@export var nekogotchi: Node3D
 @export var game_screen: Control
 
 var game_state: GameStateType = GameStateType.INTRO
@@ -15,14 +15,24 @@ enum GameStateType {
 func _ready() -> void:
 	game_screen.visible = false
 	var tween: Tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(nekogotchi_root, "rotation:y", 0.0, 2.0)
-	await tween.parallel().tween_property(nekogotchi_root, "position:y", 0.0, 2.0).finished
-	await get_tree().create_timer(1).timeout
+	tween.tween_property(nekogotchi, "rotation:y", 0.0, 2.0)
+	await tween.parallel().tween_property(nekogotchi, "position:y", 0.0, 2.0).finished
+	nekogotchi.button_a_pressed.connect(play_intro)
+	nekogotchi.button_b_pressed.connect(play_intro)
+	nekogotchi.button_c_pressed.connect(play_intro)
+
+	
+func play_intro() -> void:
+	nekogotchi.button_a_pressed.disconnect(play_intro)
+	nekogotchi.button_b_pressed.disconnect(play_intro)
+	nekogotchi.button_c_pressed.disconnect(play_intro)
 	Speaker.intro_song()
 	intro_animation.play("intro")
 	intro_animation.animation_finished.connect(start_game)
+
 
 func start_game(_anim_name: String) -> void:
 	intro_animation.animation_finished.disconnect(start_game)
 	await get_tree().create_timer(1).timeout
 	game_screen.visible = true
+	Speaker.sfx_alert()
--- scripts/nekogotchi_controller.gd
+++ scripts/nekogotchi_controller.gd
@@ -5,6 +5,10 @@ var tween_a: Tween
 var tween_b: Tween
 var tween_c: Tween
 
+signal button_a_pressed
+signal button_b_pressed
+signal button_c_pressed
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("tama_button_a"):
 		button_press(0)
@@ -22,22 +26,25 @@ func button_press(buttonID: int) -> void:
 				tween_a.kill()
 			buttons[buttonID].position.y = 0.0
 			tween_a = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-			tween_a.tween_property(buttons[buttonID], "position:y", -0.05, 0.2)
-			tween_a.tween_property(buttons[buttonID], "position:y", 0.0, 0.2)
+			tween_a.tween_property(buttons[buttonID], "position:y", -0.07, 0.1)
+			tween_a.tween_property(buttons[buttonID], "position:y", 0.0, 0.1)
+			button_a_pressed.emit()
 		1:
 			if (tween_b):
 				tween_b.kill()
 			buttons[buttonID].position.y = 0.0
 			tween_b = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-			tween_b.tween_property(buttons[buttonID], "position:y", -0.05, 0.2)
-			tween_b.tween_property(buttons[buttonID], "position:y", 0.0, 0.2)
+			tween_b.tween_property(buttons[buttonID], "position:y", -0.07, 0.1)
+			tween_b.tween_property(buttons[buttonID], "position:y", 0.0, 0.1)
+			button_b_pressed.emit()
 		2:
 			if (tween_c):
 				tween_c.kill()
 			buttons[buttonID].position.y = 0.0
 			tween_c = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-			tween_c.tween_property(buttons[buttonID], "position:y", -0.05, 0.2)
-			tween_c.tween_property(buttons[buttonID], "position:y", 0.0, 0.2)
+			tween_c.tween_property(buttons[buttonID], "position:y", -0.07, 0.1)
+			tween_c.tween_property(buttons[buttonID], "position:y", 0.0, 0.1)
+			button_c_pressed.emit()
 
 func button_press_3d(_camera: Node, event: InputEvent, _event_position: Vector3 = Vector3.ZERO, _normal: Vector3 = Vector3.ZERO, _shape_idx: int = 0, buttonID: int = 0) -> void:
 	if event.is_pressed():
